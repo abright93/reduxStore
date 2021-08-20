c144d4daf7fc085884f84373d2db36a46f22f741
Alex Bright <abrighttt@gmail.com>
2021-08-19 22:09:22 -0400

    fixing bugs

---
+++ client/build/precache-manifest.c7289f98d0ac314015d3e570a6f5c9d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01ba86b0320361db44cde83ebe34931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad03e2efb0c92123318c",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "877423b40b1673ebf1ea",
+    "url": "/static/js/2.263dfdb4.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.263dfdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad03e2efb0c92123318c",
+    "url": "/static/js/main.49188d73.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -2,3 +2,4 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 
 export default createStore(reducers);
+
